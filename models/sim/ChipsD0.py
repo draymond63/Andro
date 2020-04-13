7ea5f2ad7a7e26fe2d5bcba818d68deffdb94712
Daniel Raymond <dan@raymond.ch>
2020-04-12 23:49:01 -0400

    Second version of D4

---
+++ models/sim/ChipsD0.py
@@ -0,0 +1,24 @@
+# AUTHOR: Daniel Raymond
+# DATE  : 2020-04-07
+# ABOUT : Initial software implementation of all the chips used in the circuit - used in AndroD5
+
+# EEPROM
+class EEPROM():
+    def __init__(self, size=64):
+        self.size = size    # Measured in kB
+        self.io_len = 8     # Able to output one byte at a time
+
+# Multiplexor
+class Multiplexor():
+    def __init(self):
+        pass
+
+# Multiplier
+class Multiplier():
+    def __init__(self):
+        pass
+
+# Multiplier
+class UpDownCounter():
+    def __init__(self, clock):
+        pass
